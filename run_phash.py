d79bca4d318dca54187f25a16fd215768a4012b8
levan92 <lingevan0208@gmail.com>
2020-09-22 12:19:40 +0800

    caching logic + timing

--- run_phash.py
+++ run_phash.py
@@ -1,3 +1,4 @@
+import time
 import math
 import argparse
 from tqdm import tqdm
@@ -16,6 +17,9 @@ parser.add_argument('directory', help='Path to root directory of images')
 parser.add_argument('--thresh', help='distance threshold (hamming distance) int between 0 and 64. Default: 10', default=10, type=int)
 parser.add_argument('--get-clusters', help='if flagged, will copy images over to <input name>_Dups_thresh{thresh} output folder in their computed clusters subdirectories', action='store_true')
 parser.add_argument('--dedup', help='if flagged, will copy images over to <input name>_deduped with images randomly sampled ', action='store_true')
+cache_group = parser.add_mutually_exclusive_group()
+cache_group.add_argument('--save', help='save encoding map (phash of images) as pkl', action='store_true')
+cache_group.add_argument('--load', help='load encoding map (phash of images) from pkl', type=str)
 args = parser.parse_args()
 
 dist_thresh = int(args.thresh)
@@ -27,18 +31,31 @@ assert root_dir.is_dir()
 out_dir = root_dir.parent / 'Dups_thresh{}'.format(dist_thresh)
 
 phasher = PHash()
-# imgpaths = [p for p in root_dir.rglob('*') if p.suffix in IMG_EXTS ]
-# encoding_map = {}
-# for imgpath in tqdm(imgpaths):
-# for imgpath in root_dir.rglob('*'):
-    # if imgpath.suffix in IMG_EXTS:
-        # encoding = phasher.encode_image(image_file=str(imgpath))
-        # encoding_map[str(imgpath)] = encoding
 
-encoding_map = phasher.encode_images(image_dir=root_dir, rglob=True)
+if args.load is not None and Path(args.load).is_file():
+    import pickle
+    encoding_map = pickle.load(open(args.load, 'rb'))
+    print(f'Encoding map loaded from pickle file: {args.load}!')
+else:
+    tic = time.perf_counter()
+    encoding_map = phasher.encode_images(image_dir=root_dir, rglob=True)
+    toc = time.perf_counter()
+    print(f'encoding duration: {toc-tic:.3f}s')
+    if args.save:
+        import pickle
+        pickle_file = f"{root_dir.stem}_encoding_map.pkl"
+        pickle.dump(encoding_map, open(pickle_file,"wb"))
+        print(f'Encoding map dumped as pickle at: {pickle_file}')
+
+tic = time.perf_counter()
 distance_map = phasher.find_duplicates(encoding_map=encoding_map, max_distance_threshold=dist_thresh, scores=True)
+toc = time.perf_counter()
+print(f'find dups duration: {toc-tic:.3f}s')
 
+tic = time.perf_counter()
 clusters = clustering(distance_map)
+toc = time.perf_counter()
+print(f'clustering duration: {toc-tic:.4f}s')
 
 print('Original number of images:', len(encoding_map))
 print('Num of clusters:', len(clusters))
